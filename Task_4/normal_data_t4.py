20d4e4d7fa6b8284f3dee8d8e544253d7ed275d1
qnn001 <89320078+qnn001@users.noreply.github.com>
2023-08-03 15:48:20 -0700

    Change normalization routine for output

--- Task_4/normal_data_t4.py
+++ Task_4/normal_data_t4.py
@@ -24,27 +24,16 @@ def read_normal_data():
 
     scaler_path = './save_model_t_4/'
 
-    data = ds.read_data()
-
-    print(data.head())
-
-    first_entry = data.iloc[0]
-    
-    print(first_entry)
-    print(first_entry[0].shape) # 500,12
-    print(first_entry[1].shape) # 500,75
+    data = ds.read_data() # N rows of (x, y) tuples, x.shape = (500,12), y.shape = (500,75)
 
     # Column 1 (leads)
 
-    #print(data.iloc[:,0][0].shape) # (500, 12)
     li_1 = data['leads'].tolist()
     num_sub = len(li_1)
-    li_1 = np.vstack(li_1)
+    li_1 = np.vstack(li_1) # shape = (N*500, 12)
     scalar_list = []
-    #final_dat = np.empty(li_1.shape)
-    #print(final_dat.shape) # (1500, 12)
 
-    path = scaler_path + 'leads_scaler_'
+    path = scaler_path + 'leads_scaler'
     scaler = MinMaxScaler()
     normalized_data = scaler.fit_transform(li_1)
     joblib.dump(scaler, path)
@@ -59,36 +48,22 @@ def read_normal_data():
         #scalar_list.append(scaler)
         final_dat[:,i] = normalized_data[:,0]
     '''
-    #
-    #print(final_dat.shape)
-    a_f = np.vsplit(normalized_data, num_sub)
-    print(a_f[0].shape) # (500, 12)
-    
+
+    a_f = np.vsplit(normalized_data, num_sub) # list of N matrices, shape = (500, 12)    
     
     # Column 2 (act)
 
-    li_2 = data['act'].tolist() # 3x500x75 -> 1x1000
-    print('length li_2',len(li_2))
-    print(li_2[0].shape)
+    li_2 = data['act'].tolist()
+    li_2 = [i.reshape(-1, 1) for i in li_2]
+    li_2 = np.vstack(li_2) # (N*500*75, 1)
 
-
-    li_2 = [i.reshape(1,-1) for i in li_2]
-    #print(np.array(li_2).reshape(num_sub,1,-1).shape)
-    print('li_2',li_2)
-    li_2 = np.vstack(li_2)
-    print('li_2',li_2.shape)
-
-    path = scaler_path + 'act_scaler_'
+    path = scaler_path + 'act_scaler'
     scaler = MinMaxScaler()
     normalized_data_label = scaler.fit_transform(li_2)
     joblib.dump(scaler, path)
 
-    print('normalized_data_label',normalized_data_label.shape)
-    print(normalized_data_label)
-
-    a_f2 = np.vsplit(normalized_data_label, num_sub)
+    a_f2 = np.vsplit(normalized_data_label, num_sub) # list of N arrays, shape = (500*75, 1)
     a_f2 = [i.reshape(500,-1) for i in a_f2]
-    print(a_f2[0].shape)
 
     df = pd.DataFrame({'leads': a_f, 'act': a_f2})
     return df
